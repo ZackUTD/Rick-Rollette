a180ec55e651ca4c50df5d9a35978e31cadcbce8
Zack Oldham <zachary.oldham@cxoserve.com>
2021-08-06 23:21:41 -0500

    decrease to 20% probability

--- RickRoulette.user.js
+++ RickRoulette.user.js
@@ -2,7 +2,7 @@ var links = document.getElementsByTagName('a');
 
 for (var i = 0, l = links.length; i < l; i++) {
     links[i].onmousedown = (event) => {
-        if (Math.random() < 0.30) {
+        if (Math.random() < 0.20) {
             window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", '_blank').focus();
             event.preventDefault();
         }
